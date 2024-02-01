99de48c94e309239b3ff68f3764e16ad23955670
Utsav <utpaliwal@deloitte.nl>
2024-02-01 16:06:11 +0100

    Imported Pandas & Numpy

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+
+import pandas as pd 
+import numpy as np
+
+#The API request is below:
+
 import requests
 
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=LCK2UZVZMWBV5J4W'
 r = requests.get(url)
 data = r.json()
 
-print(type(data['Time Series (5min)']))
+print(data['Time Series (5min)'].keys())
+
+
