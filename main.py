88bed0a941b5b7fb30d850761b6aa979ca7ff9f4
Muneeb <munijaz@deloitte.nl>
2024-02-01 16:34:11 +0100

    Playing around with the API

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 
-import pandas as pd 
+import pandas as pd
 import numpy as np
 
 #The API request is below:
 
 import requests
 
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=LCK2UZVZMWBV5J4W'
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=ONZOU16MALIX7UNO'
 r = requests.get(url)
 data = r.json()
 
-print(data['Time Series (5min)'].keys())
+##print(data.keys())
+##print(type(data))
+
+#print(data['Time Series (5min)']['2024-01-31 19:55:00'].keys())
+#print(data['Time Series (5min)']['2024-01-31 19:50:00'].keys())
+
+df = pd.DataFrame(data, index = [0])
+print(df)
+
 
 
