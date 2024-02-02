8dcc09024cb26cc027c8a21561d170e5eff24fe0
Muneeb <munijaz@deloitte.nl>
2024-02-02 13:43:43 +0100

    Added Stock class with ticker, name and buy/sell methods

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
+# Importing the right packages
+
 import pandas as pd
 import numpy as np
-
-#The API request is below:
-
 import requests
+# The API request is below:
 
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=5min&apikey=ONZOU16MALIX7UNO'
-r = requests.get(url)
-data = r.json()
-
-##print(data.keys())
-##print(type(data))
-
-#print(data['Time Series (5min)']['2024-01-31 19:55:00'].keys())
-#print(data['Time Series (5min)']['2024-01-31 19:50:00'].keys())
-
-df = pd.DataFrame(data['Time Series (5min)'])
-print(df)
-
+## --- Classes ---
 
 class User:
-   
+
     def __init__(self,name,ID,opening_balance,balance,portfolio):
-        self.name = name 
+        self.name = name
         self.ID = ID
         self.opening_balance = opening_balance
         self.balance = balance
@@ -32,19 +20,61 @@ class User:
         stock_to_buy = input("Please enter the name of the stock you would like to purchase ")
         quanity_of_stock = input("Please enter the number of stocks you would like to purchase ")
         self.add_stock_to_portfolio(stock_to_buy,quanity_of_stock)
-    
+
     def add_stock_to_portfolio(self,stock,quantity):
         quantity_of_stock += quantity
-        self.portfolio.append((stock,quantity))    
+        self.portfolio.append((stock,quantity))
 
     def sell(self):
         stock_to_sell = input("Please enter the name of the stock you would like to sell ")
         Quanity_of_stock = input("Please enter the number of stocks you would like to sell ")
+class Stock:
+    def __init__(self,ticker):
+        self.ticker = ticker
+        self.name = self.get_stock_name()
+        self.buy_price = self.get_buy_price() # price at 31/12/23
+        self.sell_price = self.get_sell_price() #price at 31/01/24
+
+    def get_stock_name(self):
+        tick = self.ticker
+        link1 = f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={tick}&apikey=4H4XGZE8HAY85MW6'
+        r1 = requests.get(link1)
+        data1 = r1.json()
+        return data1['bestMatches'][0]['2. name']
+
+    def get_buy_price(self):
+        tick = self.ticker
+        link2 = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={tick}&apikey=4H4XGZE8HAY85MW6'
+        r2 = requests.get(link2)
+        data2 = r2.json()
+        return data2['Monthly Time Series']['2023-12-29']['4. close'] # price at the end of december
+
+    def get_sell_price(self):
+        tick = self.ticker
+        link2 = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={tick}&apikey=4H4XGZE8HAY85MW6'
+        r2 = requests.get(link2)
+        data2 = r2.json()
+        return data2['Monthly Time Series']['2024-02-01']['4. close'] # price at the end of december
+
+
+
 
 u1 = User("Utsav Paliwal",1,10000,0,[{}])
 
 #u1.add_stock_to_portfolio()
 
-print(u1.name,u1.opening_balance,u1.portfolio)
-u1.buy()
-u1.sell()
+#print(u1.name,u1.opening_balance,u1.portfolio)
+#u1.buy()
+#u1.sell()
+while 1:
+    user = input("Enter 1 if you know the ticker of the stock. Otherwise, enter 2 to search for a stock by keyword.")
+    if user == '1':
+        try:
+            target_stock = input("Please enter your stock ticker.")
+            stock1 = Stock(target_stock)
+            print(f'You chose stock {stock1.name} with ticker, {stock1.ticker}. The buying price of {stock1.ticker} is {stock1.buy_price}')
+        except IndexError:
+            print("No such stock found. Exiting Program. Please try again")
+            break
+    elif user == '2':
+        print("Stock not found!")
---
+++ test.py
@@ -0,0 +1,157 @@
+
+import pandas as pd
+import numpy as np
+import requests
+
+class Stock:
+    def __init__(self,ticker):
+        self.ticker = ticker
+        self.name = self.get_stock_name()
+        self.buy_price = self.get_buy_price() # price at 31/12/23
+        self.sell_price = self.get_sell_price() #price at 31/01/24
+
+    def get_stock_name(self):
+        tick = self.ticker
+        link1 = f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={tick}&apikey=4H4XGZE8HAY85MW6'
+        r1 = requests.get(link1)
+        data1 = r1.json()
+        return data1['bestMatches'][0]['2. name']
+
+    def get_buy_price(self):
+        tick = self.ticker
+        link2 = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={tick}&apikey=4H4XGZE8HAY85MW6'
+        r2 = requests.get(link2)
+        data2 = r2.json()
+        return data2['Monthly Time Series']['2023-12-29']['4. close'] # price at the end of december
+
+    def get_sell_price(self):
+        tick = self.ticker
+        link2 = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={tick}&apikey=4H4XGZE8HAY85MW6'
+        r2 = requests.get(link2)
+        data2 = r2.json()
+        return data2['Monthly Time Series']['2024-02-01']['4. close'] # price at the end of december
+
+
+user = input("Enter 1 if you know the ticker of the stock. Otherwise, enter 2 to search for a stock by keyword.")
+if user == 1:
+    target_stock = input("Please enter your stock ticker.")
+    stock1 = Stock(target_stock)
+    print(f'You chose stock {stock1.name} with ticker, {stock1.ticker}. The buying price of {stock1.ticker} is {stock1.buy_price}')
+elif user == 2:
+
+    import pandas as pd
+    import numpy as np
+    import requests
+
+    # The API request is below:
+
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=5min&apikey=4H4XGZE8HAY85MW6'
+    r = requests.get(url)
+    data = r.json()
+    print(data)
+    # print(data.keys())
+    # print(type(data))
+
+    # print(data['Time Series (5min)']['2024-01-31 19:55:00'].keys())
+    # print(data['Time Series (5min)']['2024-01-31 19:50:00'].keys())
+
+    df = pd.DataFrame(data['Time Series (5min)'])
+    print(df['2024-02-01 19:55:00'])
+
+
+    ## --- Classes ---
+
+    class Stock:
+        def __init__(self, ticker):
+            self.ticker = ticker
+            self.name = self.get_stock_name()
+
+        # self.buy_price = get_buy_price()
+        # self.sell_price = get_sell_price()
+
+        def get_stock_name(self):
+            tick = self.ticker
+            link = f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={tick}&apikey=4H4XGZE8HAY85MW6'
+            r1 = requests.get(link)
+            data1 = r1.json()
+            return data1['bestMatches'][0]['2. name']
+
+
+    stock1 = Stock('AAPL')
+    print(stock1.name)
+
+
+
+
+
+import pandas as pd
+import numpy as np
+import requests
+# The API request is below:
+
+
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=5min&apikey=ONZOU16MALIX7UNO'
+
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=5min&apikey=4H4XGZE8HAY85MW6'
+r = requests.get(url)
+data = r.json()
+
+##print(data.keys())
+##print(type(data))
+
+#print(data['Time Series (5min)']['2024-01-31 19:55:00'].keys())
+#print(data['Time Series (5min)']['2024-01-31 19:50:00'].keys())
+
+df = pd.DataFrame(data['Time Series (5min)'])
+print(df)
+df = pd.DataFrame(data['Time Series (5min)'])
+print(df['2024-02-01 19:55:00'])
+
+
+## --- Classes ---
+
+class Stock:
+    def __init__(self,ticker):
+        self.ticker = ticker
+        self.name = self.get_stock_name()
+       # self.buy_price = get_buy_price()
+        # self.sell_price = get_sell_price()
+
+    def get_stock_name(self):
+        tick = self.ticker
+        link = f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={tick}&apikey=4H4XGZE8HAY85MW6'
+        r1 = requests.get(link)
+        data1 = r1.json()
+        return data1['bestMatches'][0]['2. name']
+
+
+class User:
+
+    def __init__(self,name,ID,opening_balance,balance,portfolio):
+        self.name = name
+        self.ID = ID
+        self.opening_balance = opening_balance
+        self.balance = balance
+        self.portfolio = []
+
+    def buy(self):
+        stock_to_buy = input("Please enter the name of the stock you would like to purchase ")
+        quanity_of_stock = input("Please enter the number of stocks you would like to purchase ")
+        self.add_stock_to_portfolio(stock_to_buy,quanity_of_stock)
+
+    def add_stock_to_portfolio(self,stock,quantity):
+        quantity_of_stock += quantity
+        self.portfolio.append((stock,quantity))
+
+    def sell(self):
+        stock_to_sell = input("Please enter the name of the stock you would like to sell ")
+        Quanity_of_stock = input("Please enter the number of stocks you would like to sell ")
+
+u1 = User("Utsav Paliwal",1,10000,0,[{}])
+
+#u1.add_stock_to_portfolio()
+
+print(u1.name,u1.opening_balance,u1.portfolio)
+u1.buy()
+u1.sell()
+
